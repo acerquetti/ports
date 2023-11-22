dd78349aa5af26494cb4908e827f0591367c5632
Alejandro Cerquetti <alej8909@gmail.com>
2023-11-22 19:35:34 +0100

    Create Port aggregate and VOs

--- domain/port.go
+++ domain/port.go
@@ -1,1 +1,37 @@
 package domain
+
+type ID string
+
+type Name string
+
+type Coordinates []float64
+
+type Location struct {
+	City     string
+	Province string
+	Country  string
+}
+
+type Aliases []string
+
+type Regions []string
+
+type Timezone string
+
+type Code string
+
+type Port struct {
+	ID          ID
+	Name        Name
+	Coordinates Coordinates
+	Location    Location
+	Alias       Aliases
+	Regions     Regions
+	Timezone    Timezone
+	Unlocs      []ID
+	Code        Code
+}
+
+type Repository interface {
+	Save(Port) error
+}
