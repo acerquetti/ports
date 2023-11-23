297921ab978a59432ad43617ddf04f40d771ef07
Alejandro Cerquetti <alej8909@gmail.com>
2023-11-23 19:06:27 +0100

    Inject dependencies from main and run server

--- cmd/ports/main.go
+++ cmd/ports/main.go
@@ -1,5 +1,47 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
 
+	"github.com/acerquetti/ports/app"
+	"github.com/acerquetti/ports/infra/controller"
+	"github.com/acerquetti/ports/infra/database"
+)
+
+var (
+	portsPath  string
+	serverAddr string
+)
+
+func init() {
+	flag.StringVar(&portsPath, "portsPath", "./ports.json", "Path pointing ports.json file")
+	flag.StringVar(&serverAddr, "serverAddr", ":8080", `Server address (e.g. ":8080")`)
+	flag.Parse()
+}
+
+func main() {
+	var err error
+
+	portsFile, err := os.Open(portsPath)
+	mustNotError(err)
+
+	db, err := database.NewMemoryDB(portsFile)
+	mustNotError(err)
+
+	err = portsFile.Close()
+	mustNotError(err)
+
+	svc := app.NewService(db)
+	ctrl := controller.NewREST(svc)
+
+	log.Fatal(http.ListenAndServe(serverAddr, ctrl))
+}
+
+func mustNotError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
