41fc2c8be9a48bc5757c70f382a62397a39d81b1
Alejandro Cerquetti <alej8909@gmail.com>
2023-11-23 18:25:26 +0100

    Add Create use-case

--- app/service.go
+++ app/service.go
@@ -1,1 +1,28 @@
 package app
+
+import (
+	"github.com/acerquetti/ports/domain"
+)
+
+type Service interface {
+	Create(domain.PortRaw) error
+}
+
+type serviceImpl struct {
+	repository domain.Repository
+}
+
+func NewService(repository domain.Repository) *serviceImpl {
+	return &serviceImpl{
+		repository: repository,
+	}
+}
+
+func (s *serviceImpl) Create(portRaw domain.PortRaw) error {
+	port, err := domain.NewPortFromRaw(portRaw)
+	if err != nil {
+		return err
+	}
+
+	return s.repository.Save(*port)
+}
